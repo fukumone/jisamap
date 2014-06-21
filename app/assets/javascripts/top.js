9ffa0c147b4f7118729a028fa0b93340b60005cc
t-fukui <fukui@rubyquitous.co.jp>
2014-06-21 22:58:16 +0900

    時差取得

--- app/assets/javascripts/top.js
+++ app/assets/javascripts/top.js
@@ -1,4 +1,5 @@
 // Google Map APIの処理
+var map;
 function initialize() {
   var tokyo = new google.maps.LatLng(35.689614,139.691585);
   var myOptions = {
@@ -30,9 +31,77 @@ function initialize() {
   // ];
   var map = new google.maps.Map(document.getElementById('map_canvas'), myOptions);
   // map.setOptions({styles: styles});
+  setClickEvent(map);
 }
+
+function setClickEvent(map)
+{
+  google.maps.event.addListener(map, 'click', function(event) {
+    var requestUrl =
+        'https://maps.googleapis.com/maps/api/timezone/' +
+        'json' +
+        '?location=' + event.latLng.lat() + ',' + event.latLng.lng() +
+        '&timestamp=' + getTimeStamp(new Date().getTime()) +
+        '&sensor=' + 'false' +
+        '&language=' + 'ja';
+
+    //request timezone
+    $.ajax({
+        url: requestUrl,
+        type: 'GET',
+        success: function(timeZone) {
+          if (timeZone['status'] == 'OK') {
+              //add marker
+              addMarker(event.latLng, timeZone, map);
+          } else {
+              //error
+              alert('status:' + timeZone['status']);
+          }
+        }
+    });
+  });
+}
+
+function getTimeStamp(time)
+{
+  return Math.round(time / 1000);
+}
+
+function addMarker(latLng, timeZone, map)
+{
+  var contentString =
+      '<div class="content">' +
+      '<p>' + '緯度経度:　' + latLng + '</p>' +
+      '<p>' + 'タイムゾーンID:　' + timeZone['timeZoneId'] + '</p>' +
+      '<p>' + 'タイムゾーン名:　' + timeZone['timeZoneName'] + '</p>' +
+      '<p>' + '時差:　' + timeZone['rawOffset']/3600 + '時間' + '</p>' +
+      '<p>' + 'サマータイムによる時差:　' + timeZone['dstOffset']/3600 + '時間' + '</p>' +
+      '</div>';
+
+  //create infowindow
+  var infowindow = new google.maps.InfoWindow({
+      content: contentString
+  });
+
+  //create marker
+  var marker = new google.maps.Marker({
+    position:latLng,
+  });
+
+  //set event
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.open(map, marker);
+  });
+
+  //set map
+  marker.setMap(map);
+
+  //open infowindow
+  infowindow.open(map, marker);
+}
+
+//initialize
 
 window.onload = function(){
   initialize();
 };
-
