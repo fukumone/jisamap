594be1cd66466a069e9c115f6bb0f3ddb17cc471
t-fukui <fukui@rubyquitous.co.jp>
2014-06-22 18:06:31 +0900

    google map デザイン変更

--- app/assets/javascripts/top.js
+++ app/assets/javascripts/top.js
@@ -3,34 +3,144 @@ var map;
 function initialize() {
   var tokyo = new google.maps.LatLng(35.689614,139.691585);
   var myOptions = {
-    zoom: 4,
+    zoom: 3,
     center: tokyo,
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     mapTypeControl: false
   };
-  // var styles = [
-  //   {
-  //     stylers: [
-  //       { hue: "#00ffe6" },
-  //       { saturation: -20 }
-  //     ]
-  //   },{
-  //     featureType: "road",
-  //     elementType: "geometry",
-  //     stylers: [
-  //       { lightness: 100 },
-  //       { visibility: "simplified" }
-  //     ]
-  //   },{
-  //     featureType: "road",
-  //     elementType: "labels",
-  //     stylers: [
-  //       { visibility: "off" }
-  //     ]
-  //   }
-  // ];
+  var styles = [
+    {
+        "featureType": "water",
+        "stylers": [
+            {
+                "saturation": 43
+            },
+            {
+                "lightness": -11
+            },
+            {
+                "hue": "#0088ff"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "hue": "#ff0000"
+            },
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 99
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#808080"
+            },
+            {
+                "lightness": 54
+            }
+        ]
+    },
+    {
+        "featureType": "landscape.man_made",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#ece2d9"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#ccdca1"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#767676"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape.natural",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#b8cb93"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.sports_complex",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.medical",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.business",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    }
+  ]
   var map = new google.maps.Map(document.getElementById('map_canvas'), myOptions);
-  // map.setOptions({styles: styles});
+  map.setOptions({styles: styles});
   setClickEvent(map);
 }
 
